6a424eb1c2387d013beced261b0c34e092e0f26b
Antonin Aumètre <antonin.aumetre@gmail.com>
2019-07-21 14:49:18 +0200

    Saving before major changes

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 class Task:
-    def __init__(self, ID, parents, machine, unitDuration):
+    def __init__(self, ID, parents, machine, start_date, unitDuration):
         self.ID = ID
         self.parents = parents
         self.machine = machine
         self.unit_duration = unitDuration
+        self.start_date = start_date
         self.done = False
 
 
@@ -24,12 +25,16 @@ class Machine:
         self.end_date = date + task.unit_duration
 
     def endTask(self, task, date):
+        self.task.done = True
         self.task = None
         self.startDate = 0
         self.endDate = 0
 
     def selectTaskToStart(self, date):
-        print("TODO: selectTaskToStart")
+        for task in self.tasks:
+            if task.done == False:
+                print(task)
+                return task
         return None
 
 
@@ -48,14 +53,15 @@ class Processor:
             self.machines[ID] = machine
         return machine
 
-    def newTask(self, ID, parents, machine, unitDuration):
-        task = Task(ID, parents, machine, unitDuration)
+    def newTask(self, ID, parents, machine, date, unitDuration):
+        task = Task(ID, parents, machine, date, unitDuration)
         machine.associateTask(task)
         return task
 
 
 class Event:
-    def __init__(self, type, task, date):
+    def __init__(self, ID, type, task, date):
+        self.ID = ID
         self.type = type
         self.task = task
         self.date = date
@@ -108,7 +114,7 @@ class Simulator:
     def __init__(self, processor):
         self.processor = processor
         self.schedule = Schedule()
-        self.print_trace = True
+        self.print_trace = False
 
     def simulationLoop(self):
         last_date = 0
@@ -121,18 +127,17 @@ class Simulator:
             last_date = event.date
         return last_date
 
+    # Effectively calls an event
     def fireEvent(self, event):
         if event.type == "start-task":
             event.task.machine.startTask(event.task, event.date)
             self.schedule.newEndTaskEvent(event.task, event.task.machine.end_date)
             if self.print_trace:
-                print(str(event.ID) + "@" + str(event.date) + ": start task " + str(event.task.ID) + " on batch " + str(
-                    event.batch.code))
+                print(str(event.ID) + "@" + str(event.date) + ": start task " + str(event.task.ID))
         else:
             event.task.machine.endTask(event.task, event.date)
             if self.print_trace:
-                print(str(event.ID) + "@" + str(event.date) + ": end task " + str(event.task.ID) + " on batch " + str(
-                    event.batch.code))
+                print(str(event.ID) + "@" + str(event.date) + ": end task " + str(event.task.ID))
 
     def lookForTaskToStartInProcessor(self, date):
         for machine in self.processor.machines.values():
@@ -144,6 +149,15 @@ class Simulator:
             return
         self.schedule.newStartTaskEvent(selection, date)
 
+    # Go through the machines, extract the tasks and add them to the schedule
+    def loadTasks(self):
+        for i in range(len(self.processor.machines)):
+            machine = self.processor.getMachine(i)
+            for task in machine.tasks:
+                this_date = task.start_date
+                self.schedule.newStartTaskEvent(task, this_date)
+                self.schedule.newEndTaskEvent(task, this_date + task.unit_duration)
+
 
 # =============================================================================
 processor = Processor()
@@ -153,19 +167,20 @@ machine1 = processor.newMachine(1)
 machine2 = processor.newMachine(2)
 machine3 = processor.newMachine(3)
 
-processor.newTask(0, [], machine0, 5)
-processor.newTask(1, [0], machine0, 5)
-processor.newTask(2, [0], machine1, 5)
-processor.newTask(3, [0], machine2, 5)
-processor.newTask(4, [0], machine3, 5)
-processor.newTask(5, [1], machine0, 5)
-processor.newTask(6, [6], machine1, 5)
-processor.newTask(7, [3], machine2, 5)
-processor.newTask(8, [3, 4], machine3, 5)
+processor.newTask(0, [], machine0, 0, 5)
+processor.newTask(1, [0], machine0, 5, 5)
+processor.newTask(2, [0], machine1, 5, 5)
+processor.newTask(3, [0], machine2, 5, 5)
+processor.newTask(4, [0], machine3, 5, 5)
+processor.newTask(5, [1], machine0, 10, 5)
+processor.newTask(6, [6], machine1, 10, 5)
+processor.newTask(7, [3], machine2, 10, 5)
+processor.newTask(8, [3, 4], machine3, 15, 5)
 
 # The simulator needs to check the execution goes as planned i.e. that a given
 # tasks' parents are done executing before it is, itself, executed
 simulator = Simulator(processor)
 
+simulator.loadTasks()
 last_date = simulator.simulationLoop()
 print("Computation completed at " + str(last_date))
